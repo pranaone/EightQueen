6c7cb90fead08f221fcdfcb87aa8c7087ae0746d
Paranthaman Pranavan <31650351+pranaone@users.noreply.github.com>
2020-02-07 19:14:40 +0530

    fixes

---
+++ EightQueenPuzzle/Queen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightQueenPuzzle
+{
+    public class Queen
+    {
+        private int row;
+        private int column;
+
+        public Queen()
+        {
+            row = 0;
+            column = 0;
+        }
+
+        public Queen(byte Row, byte Column)
+        {
+            row = Row;
+            column = Column;
+        }
+
+        public int Row
+        {
+            get
+            {
+                return row;
+            }
+            set
+            {
+                row = value;
+            }
+        }
+
+        public int Column
+        {
+            get
+            {
+                return column;
+            }
+            set
+            {
+                column = value;
+            }
+        }
+    }
+}
